8be1117fb6ed826c99f36e3455b056eee6c8ca9f
yanisimova <yanisimova@magnite.com>
2022-03-29 21:15:14 +0300

    send request test

---
+++ src/main/java/utils/SendRequest.java
@@ -0,0 +1,41 @@
+package utils;
+
+import com.squareup.okhttp.HttpUrl;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+
+import java.io.IOException;
+
+public class SendRequest {
+
+    public static void main(String[] args) {
+        sendRequestToUrl();
+    }
+
+
+    private static final OkHttpClient client = new OkHttpClient();
+
+    public static void sendRequestToUrl() {
+        HttpUrl url = new HttpUrl.Builder()
+                .scheme("https")
+                .host("google.com")
+                .build();
+
+        Request request = buildRequest(url);
+
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response.code());
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+    }
+
+    public static Request buildRequest(HttpUrl url) {
+        return new Request.Builder()
+                .url(url)
+                .build();
+    }
+}
