a7d7124dc220aea44f58d0e9cbf9f3aba81ad24d
yanisimova <yanisimova@magnite.com>
2022-03-31 20:17:23 +0300

    redirect to

--- src/main/java/utils/SendRequest.java
+++ src/main/java/utils/SendRequest.java
@@ -7,36 +7,41 @@ import com.squareup.okhttp.Response;
 
 
 import java.io.IOException;
+import java.net.URL;
 
 public class SendRequest {
+
+    private static final String REQUEST_HOST = "inweb.ua"; //можна потім винести до aplication.proprty файлу.
+    private static final String PARAM_URL = "seo";
 
     public static void main(String[] args) {
         sendRequestToUrl();
     }
-
 
     private static final OkHttpClient client = new OkHttpClient();
 
     public static void sendRequestToUrl() {
         HttpUrl url = new HttpUrl.Builder()
                 .scheme("https")
-                .host("inweb.ua")
-                .addPathSegment("seo")
+                .host(REQUEST_HOST)
+                .addPathSegment(PARAM_URL)
                 .build();
 
+        client.setFollowRedirects(false);
+
+        //Create request
         Request request = buildRequest(url);
 
         try {
             Response response = client.newCall(request).execute();
             if (response.code() == 301) {
-                System.out.println(response.code());
-                System.out.println("Redirect to " + response.header("location"));
+                System.out.println("Status code is " + response.code() + " for " + response.request().url() + " Redirect to " + response.header("location"));
+                url = HttpUrl.get(new URL(response.header("location")));
+                response = client.newCall(buildRequest(url)).execute();
             }
-
-            System.out.println(response.code());
-            System.out.println(response.request().url());
+            System.out.println("Status code is " + response.code() + " for " + response.request().url());
         } catch (IOException e) {
-            System.out.println("Error");
+            System.out.println("Error. Incorrect host or some else parameters. Any response was get");
         }
     }
 
